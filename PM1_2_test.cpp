832955d0a2d2047983f08a01992f526a89a693ae
匠人 山口 <k023g0005@g.neec.ac.jp>
2023-10-18 11:49:56 +0900

    Playerを作成

--- PM1_2_test.cpp
+++ PM1_2_test.cpp
@@ -7,7 +7,9 @@ int main()
 {
     std::cout << "Hello World!\n";
     std::cout << "LC1A_27\n";
-    std::cout << "2023/10/11";
+    std::cout << "2023/10/11\n";
+
+    std::cout << "player作成\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
